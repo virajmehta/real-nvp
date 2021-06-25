99e8cc6bf397f58df3a301047c7ba49cc059efbe
Viraj Mehta <virajm@cs.cmu.edu>
2021-06-25 10:19:27 -0400

    wip

--- models/__init__.py
+++ models/__init__.py
@@ -1,2 +1,3 @@
 from models.real_nvp import RealNVP, RealNVPLoss
-from models.mlp_acvae import MLP_ACVAE, VAELoss
+from models.mlp_acvae import MLP_ACVAE
+from models.vae import VAE, VAELoss, VAELossCE
